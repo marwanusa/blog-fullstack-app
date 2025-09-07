8077fed8ecda975c5a13876c3bf763cb4dcbe4f4
marwanusa <professionalma42@gmail.com>
2025-09-07 20:43:34 +0300

    Add error handling middleware for improved error management

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./config/connectToDB");
+const { notFound, errorHandler } = require("./middlewares/error");
 
 // Connect to DP
 connectToDB();
@@ -17,6 +18,10 @@ app.use("/api/users", require("./routes/users.route"));
 app.use("/api/posts", require("./routes/posts.route"));
 app.use("/api/comments", require("./routes/comments.route"));
 app.use("/api/categories", require("./routes/categories.route"));
+
+// Error Handler Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 // Running the server
 const PORT = process.env.PORT || 8000;
